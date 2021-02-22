abb8679e4e3bb8761b1e30b1abb977347e1a7649
Alessandro Mulloni <ale@mulloni.tech>
2021-02-22 10:25:55 +0100

    Updated package definition

--- Package.swift
+++ Package.swift
@@ -1,7 +1,15 @@
 // swift-tools-version:5.0
-
 import PackageDescription
 
 let package = Package(
-    name: "Licenses"
+    name: "Licenses",
+    platforms: [
+        .iOS(.v11), .macOS(.v10_11)
+    ],
+    products: [
+        .library(name: "Licenses", targets: [ "Licenses" ]),
+    ],
+    targets: [
+        .target(name: "Licenses", dependencies: [ ], path: "Licenses/Sources")
+    ]
 )
